b341b27b1749f3b5b4e788dabbdb65698505bba0
Calemi <lemzsteamerson@gmail.com>
2016-07-03 21:17:11 -0400

    Update changes

--- src/main/java/tm/fissionwarfare/Reference.java
+++ src/main/java/tm/fissionwarfare/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "FissionWarfare";
 	public static final String MOD_NAME = "Fission Warfare";
-	public static final String VERSION = "1.7.10-1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String CLIENT_PROXY_CLASS = "tm.fissionwarfare.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "tm.fissionwarfare.proxy.ServerProxy";
 	public static final String GUI_FACTORY_CLASS = "tm.fissionwarfare.gui.base.GuiFactory";
